61186f708fd5e1d86a185756bbf002a631c8598c
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-05-17 16:20:49 -0300

    feat: create auth assignor function

---
+++ apps/frontend/src/app/(assignor)/api/assignor.ts
@@ -0,0 +1,11 @@
+"use client"
+const assignorUrls = {
+  create: '/integrations/auth'
+}
+
+const loginAssignor = async ({ login, password }: { login: string, password: string }) => {
+  const { error, data } = await apiClient.post<{token: string}>(assignorUrls.create, { login, password })
+  if (error) throw new Error('Error handling not implemented!', { cause: error })
+
+  console.log(data)
+}
