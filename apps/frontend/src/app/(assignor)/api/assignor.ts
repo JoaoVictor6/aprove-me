3dd4967be417d481b48bcbe8eaeeb15739fba9cb
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-05-17 16:22:00 -0300

    refactor: rename loginAssignor function to authAssignor

--- apps/frontend/src/app/(assignor)/api/assignor.ts
+++ apps/frontend/src/app/(assignor)/api/assignor.ts
@@ -3,7 +3,7 @@ const assignorUrls = {
   create: '/integrations/auth'
 }
 
-const loginAssignor = async ({ login, password }: { login: string, password: string }) => {
+const authAssignor = async ({ login, password }: { login: string, password: string }) => {
   const { error, data } = await apiClient.post<{token: string}>(assignorUrls.create, { login, password })
   if (error) throw new Error('Error handling not implemented!', { cause: error })
 
