9bb0a821acd89d4f76e52cfdeaf10b0a447c43b8
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-05-19 16:01:18 -0300

    refactor: remove unnecessary import on app.module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IntegrationsModule } from './integrations/integrations.module';
 
 @Module({
-  imports: [
-    IntegrationsModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
-    }),
-  ],
+  imports: [IntegrationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
