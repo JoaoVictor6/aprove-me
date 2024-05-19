35f459d16011662620640750b465e50bc79dd57d
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-05-19 15:55:34 -0300

    fix: env not reading

--- apps/backend/src/integrations/integrations.module.ts
+++ apps/backend/src/integrations/integrations.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { IntegrationsController } from './integrations.controller';
 import { IntegrationsService } from './integrations.service';
 
+ConfigModule.forRoot();
 @Module({
   providers: [IntegrationsService],
   controllers: [IntegrationsController],
+  imports: [
+    JwtModule.register({
+      secretOrPrivateKey: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+    }),
+  ],
 })
 export class IntegrationsModule {}
