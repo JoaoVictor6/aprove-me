0c31991479d6d4f9483f88a31e794b83077f26be
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-05-20 16:00:25 -0300

    test: add prisma service provider on integrations service test file

--- apps/backend/src/integrations/integrations.service.spec.ts
+++ apps/backend/src/integrations/integrations.service.spec.ts
@@ -1,13 +1,14 @@
 import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { IntegrationsService } from './integrations.service';
+import { PrismaService } from './prisma.service';
 
 describe('IntegrationsService', () => {
   let service: IntegrationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [IntegrationsService],
+      providers: [IntegrationsService, PrismaService],
       imports: [
         JwtModule.register({
           secret: process.env.JWT_SECRET,
