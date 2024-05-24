776de411a51a2e7f7c3161395b551db1e30bd36b
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-05-24 10:11:11 -0300

    chore: config react-testing-library

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -1,10 +1,13 @@
 // vitest.config.ts
+import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.js'],
   },
-  plugins: [tsconfigPaths()]
+  plugins: [tsconfigPaths(), react()]
 })
